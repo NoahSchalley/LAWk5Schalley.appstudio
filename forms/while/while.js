efe5984ad5376517d1788628bc50053c8d82f61e
NoahSchalley <70349070+NoahSchalley@users.noreply.github.com>
2021-03-03 14:39:51 -0600

    Commit

---
+++ forms/while/while.js
@@ -0,0 +1,9 @@
+let runLoop = "Y"
+i = 0 
+
+while (runLoop == "Y") {
+    runLoop = prompt("Do you want to continue to run the loop Y/N)") 
+    console.log(`This is loop iteration ${i}.`)
+    i++
+}
+
